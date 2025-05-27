2fb90c53d55bf3b928a7fdd22affa6fe3a3955bd
misproyectosdeveloper <misproyectosgithubdeveloper@gmail.com>
2025-05-27 11:12:43 -0300

    Add files via upload
    Arranque se semana

---
+++ mi_api4.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+DATABASE = 'db.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row  # para acceder a las columnas por nombre
+    return conn
+
+@app.route('/articulos', methods=['GET'])
+def get_articulos():
+    conn = get_db_connection()
+    cursor = conn.execute('SELECT * FROM articulos')
+    articulos = [dict(row) for row in cursor.fetchall()]
+    conn.close()
+    return jsonify(articulos)
+
+@app.route('/clientes', methods=['GET'])
+def get_articulos():
+    conn = get_db_connection()
+    cursor = conn.execute('SELECT * FROM clientes')
+    clientes = [dict(row) for row in cursor.fetchall()]
+    conn.close()
+    return jsonify(clientes)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
